efd72b281cd9c68c8c1c609172ecffec11e11de6
Jullan <jmihkkal_lindi@hotmail.com>
2019-11-15 18:59:48 +0100

    Shared memory parallelization functional

--- main.cu
+++ main.cu
@@ -11,13 +11,14 @@ extern "C" {
 #define ERROR_EXIT -1
 
 /* Divide the problem into blocks of BLOCKX x BLOCKY threads */
-#define BLOCKY 8
-#define BLOCKX 8
+#define BLOCKY 32
+#define BLOCKX 32
 
 
 /* Problem size */
 //#define XSIZE 2560
 //#define YSIZE 2048
+#define FILTERDIM 3
 
 // Generalize PIXEL(i,j) to support every horizontal res
 #define PIXEL(i,j,width) ((i)+(j)*(width))
@@ -38,6 +39,13 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 // If you apply another filter, remember not only to exchange
 // the filter but also the filterFactor and the correct dimension.
 
+int const laplacian1Filter[] = {  -1,  -4,  -1,
+                                 -4,  20,  -4,
+                                 -1,  -4,  -1};
+
+float const laplacian1FilterFactor = (float) 1.0;
+
+/*
 int const sobelYFilter[] = {-1, -2, -1,
                              0,  0,  0,
                              1,  2,  1};
@@ -47,13 +55,6 @@ int const sobelXFilter[] = {-1, -0, -1,
                             -2,  0, -2,
                             -1,  0, -1 , 0};
 float const sobelXFilterFactor = (float) 1.0;
-
-
-int const laplacian1Filter[] = {  -1,  -4,  -1,
-                                 -4,  20,  -4,
-                                 -1,  -4,  -1};
-
-float const laplacian1FilterFactor = (float) 1.0;
 
 int const laplacian2Filter[] = { 0,  1,  0,
                                  1, -4,  1,
@@ -73,9 +74,9 @@ int const gaussianFilter[] = { 1,  4,  6,  4, 1,
                                6, 24, 36, 24, 6,
                                4, 16, 24, 16, 4,
                                1,  4,  6,  4, 1 };
-
+x
 float const gaussianFilterFactor = (float) 1.0 / 256.0;
-
+*/
 
 // Apply convolutional filter on image data (CPU - serial version)
 void host_applyFilter(unsigned char **out, unsigned char **in, unsigned int width, unsigned int height, int *filter, unsigned int filterDim, float filterFactor) {
@@ -104,7 +105,7 @@ void host_applyFilter(unsigned char **out, unsigned char **in, unsigned int widt
   }
 }
 
-// Apply convolutional filter on image data (GPU - device kernel function)
+// Apply convolutional filter on image data (GPU - basic device kernel function)
 __global__ void device_applyFilter(unsigned char *out, unsigned char *in, unsigned int width, unsigned int height, int *filter, unsigned int filterDim, float filterFactor) {
 	unsigned int const filterCenter = (filterDim / 2);
 	unsigned int y = blockIdx.y * BLOCKY + threadIdx.y;
@@ -125,11 +126,56 @@ __global__ void device_applyFilter(unsigned char *out, unsigned char *in, unsign
 	aggregate *= filterFactor;
 	if (aggregate > 0) {
 		out[PIXEL(x, y, width)] = (aggregate > 255) ? 255 : aggregate;
-	} else {
+	}
+	else {
 		out[PIXEL(x, y, width)]= 0;
 	}
 }
 
+// Apply convolutional filter on image data (GPU - device kernel function using shared memory)
+__global__ void s_device_applyFilter(unsigned char *out, unsigned char *in, unsigned int width, unsigned int height, int *filter, unsigned int filterDim, float filterFactor) {
+	unsigned int const filterCenter = (filterDim / 2);
+	unsigned int y = blockIdx.y * BLOCKY + threadIdx.y;
+	unsigned int x = blockIdx.x * BLOCKX + threadIdx.x;
+	
+
+	__shared__ int s_filter[FILTERDIM * FILTERDIM];
+	if (threadIdx.x < filterDim && threadIdx.y < filterDim) {
+			s_filter[PIXEL(threadIdx.x, threadIdx.y, filterDim)] = filter[PIXEL(threadIdx.x, threadIdx.y, filterDim)];
+	}
+
+	//__shared__ unsigned char s_in[BLOCKX * BLOCKY];
+	//s_in[PIXEL(threadIdx.x, threadIdx.y , BLOCKX)] = in[PIXEL(x, y, width)];
+
+
+	int aggregate = 0;
+	__syncthreads();
+	for (unsigned int ky = 0; ky < filterDim; ky++) {
+		int nky = filterDim - 1 - ky;
+		int dy = ky - filterCenter;
+		int yy = y + dy;
+		for (unsigned int kx = 0; kx < filterDim; kx++) {
+			int nkx = filterDim - 1 - kx;
+			int dx = kx - filterCenter;
+			//if (threadIdx.x >= filterCenter && threadIdx.x < BLOCKX - filterCenter && threadIdx.y >= filterCenter && threadIdx.y < BLOCKY - filterCenter) {
+			//	aggregate += s_in[PIXEL(threadIdx.x + dx, threadIdx.y + dy , BLOCKX)] * s_filter[nky * filterDim + nkx];
+			//	continue;
+			//}
+
+			int xx = x + dx;
+			if (xx >= 0 && xx < (int) width && yy >=0 && yy < (int) height) {
+				aggregate += in[PIXEL(xx, yy, width)] * s_filter[nky * filterDim + nkx];
+			}
+		}
+	}
+	aggregate *= filterFactor;
+	if (aggregate > 0) {
+		out[PIXEL(x, y, width)] = (aggregate > 255) ? 255 : aggregate;
+	}
+	else {
+		out[PIXEL(x, y, width)]= 0;
+	}
+}
 
 void help(char const *exec, char const opt, char const *optarg) {
     FILE *out = stdout;
@@ -245,6 +291,8 @@ int main(int argc, char **argv) {
 		freeBmpImageChannel(imageChannel);
 		return ERROR_EXIT;
 	}
+	int kerDim = FILTERDIM; // Dimension of currently used kernel
+
 	// Assign image dimensions onto variables
 	int im_XSIZE = imageChannel->width;
 	int im_YSIZE = imageChannel->height;
@@ -254,15 +302,43 @@ int main(int argc, char **argv) {
 	double hosttime=0;
 	double devicetime=0;
 
+	/*																					*/
+	/*									CPU PROCESSING									*/
+	/*																					*/
+
 	// Intitialize host memory for CPU processing.
 	// imageChannel->data is a 2-dimensional array of unsigned char which is accessed row first ([y][x]).
 	bmpImageChannel *processImageChannel = newBmpImageChannel(im_XSIZE, im_YSIZE);
 
-	// Initialize host memory for GPU processing
-	bmpImageChannel *gpuImageChannel = newBmpImageChannel(im_XSIZE, im_YSIZE);
+	//Here we do the actual computation!
+	// Host computation
+	start = walltime();
+	for (unsigned int i = 0; i < 0; i ++) {
+		host_applyFilter(processImageChannel->data,
+				imageChannel->data,
+				im_XSIZE,
+				im_YSIZE,
+				(int *)laplacian1Filter, kerDim, laplacian1FilterFactor
+	//               (int *)laplacian2Filter, 3, laplacian2FilterFactor
+	//               (int *)laplacian3Filter, 3, laplacian3FilterFactor
+	//               (int *)gaussianFilter, 5, gaussianFilterFactor
+				);
+		//Swap the data pointers
+		unsigned char ** tmp1 = processImageChannel->data;
+		processImageChannel->data = imageChannel->data;
+		imageChannel->data = tmp1;
+		unsigned char * tmp_raw = processImageChannel->rawdata;
+		processImageChannel->rawdata = imageChannel->rawdata;
+		imageChannel->rawdata = tmp_raw;
+	}
+	hosttime = walltime() - start;
 
-	int kerDim = 3; // Dimension of currently used kernel
+	// Free host memory
+	freeBmpImageChannel(processImageChannel);
 
+	/*																					*/
+	/*									GPU PROCESSING									*/
+	/*																					*/
 	// Initialize device memory for GPU processing.
 	unsigned char *devChannel; // Resultant array
 	unsigned char *devProcChannel; // Imageprocessing array
@@ -278,38 +354,13 @@ int main(int argc, char **argv) {
 	// Copy the kernel data from host to device memory.
 	cudaErrorCheck(cudaMemcpy(devKernel, laplacian1Filter, kerDim * kerDim * sizeof(int), cudaMemcpyHostToDevice));
 
-	//Here we do the actual computation!
-	// Host computation
-	start = walltime();
-	for (unsigned int i = 0; i < iterations; i ++) {
-		host_applyFilter(processImageChannel->data,
-				imageChannel->data,
-				im_XSIZE,
-				im_YSIZE,
-				(int *)laplacian1Filter, 3, laplacian1FilterFactor
-	//               (int *)laplacian2Filter, 3, laplacian2FilterFactor
-	//               (int *)laplacian3Filter, 3, laplacian3FilterFactor
-	//               (int *)gaussianFilter, 5, gaussianFilterFactor
-				);
-		//Swap the data pointers
-		unsigned char ** tmp1 = processImageChannel->data;
-		processImageChannel->data = imageChannel->data;
-		imageChannel->data = tmp1;
-		unsigned char * tmp_raw = processImageChannel->rawdata;
-		processImageChannel->rawdata = imageChannel->rawdata;
-		imageChannel->rawdata = tmp_raw;
-	}
-	hosttime += walltime() - start;
-
-	// Free host memory
-	freeBmpImageChannel(processImageChannel);
+	dim3 gridBlock(im_XSIZE / BLOCKX, im_YSIZE / BLOCKY);
+	dim3 threadBlock(BLOCKX, BLOCKY);
 
 	// Device computation
 	start = walltime();
 	for (unsigned int i = 0; i < iterations; i ++) {
-		dim3 gridBlock(im_XSIZE / BLOCKX, im_YSIZE / BLOCKY);
-		dim3 threadBlock(BLOCKX, BLOCKY);
-		device_applyFilter<<<gridBlock, threadBlock>>>(devProcChannel,
+		s_device_applyFilter<<<gridBlock, threadBlock>>>(devProcChannel,
 					devChannel,
 					im_XSIZE,
 					im_YSIZE,
@@ -322,10 +373,13 @@ int main(int argc, char **argv) {
 		devProcChannel = devChannel;
 		devChannel = tmp2;
 	}
-	devicetime += walltime() - start;
+	devicetime = walltime() - start;
 	// Free device memory used in processing
 	cudaFree(devProcChannel);
 	cudaFree(devKernel);
+
+	// Initialize host memory for GPU processed image
+	bmpImageChannel *gpuImageChannel = newBmpImageChannel(im_XSIZE, im_YSIZE);
 
 	// Copy processed image memory from device to host
 	cudaMemcpy(gpuImageChannel->rawdata, devChannel, im_XSIZE * im_YSIZE, cudaMemcpyDeviceToHost);
